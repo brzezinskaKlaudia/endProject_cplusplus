c1d4b855526eeaba6424ca24ca6ae7f8792142a8
klaudidaBrzezinska <brzezinska.klaudia93@gmail.com>
2022-04-19 18:53:57 +0200

    Initial Commit

---
+++ end_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  end_project
+//
+//  Created by Klaudia Brzezińska on 19.04.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
